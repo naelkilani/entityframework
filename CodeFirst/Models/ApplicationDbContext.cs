5ce1027751c9833076bf52c0babe791172a3f163
naelkailani <naelkilanii@gmail.com>
2020-05-17 16:51:50 +0300

    first migration

--- CodeFirst/Models/ApplicationDbContext.cs
+++ CodeFirst/Models/ApplicationDbContext.cs
@@ -4,6 +4,10 @@ namespace CodeFirst.Models
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
         public ApplicationDbContext()
         : base("name=DefaultConnection")
         {
